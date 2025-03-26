31915be1f6d7207e8f174f83c722b9c93ce22d39
Andrei <ionascuandrei320@gmail.com>
2025-03-26 21:01:39 +0200

    Initial commit: Created the project with Docker

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BacPeBune.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
