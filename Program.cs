5aed3b201f32d6aab56a0338a5cc28bdb31182d0
matei21 <43905663+matei21@users.noreply.github.com>
2025-06-06 15:33:50 +0300

    Update - intro to seeding the database

--- Program.cs
+++ Program.cs
@@ -7,23 +7,12 @@ using System.IO;
 using Microsoft.Extensions.Logging;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 using System.Data.Common;
 using MySqlConnector;  
-
-
-=======
-=======
->>>>>>> Stashed changes
 using Microsoft.AspNetCore.DataProtection; 
 using System.IO;
 using Microsoft.Extensions.Logging;      
 using BacPeBune.Models;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -72,46 +61,32 @@ else
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    var context = services.GetRequiredService<ApplicationDbContext>();
     try
     {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        var context = services.GetRequiredService<ApplicationDbContext>();
         context.Database.Migrate();
-        //await SeedData.Initialize(services); 
-=======
-        context.Database.Migrate();
-=======
-        context.Database.Migrate();
->>>>>>> Stashed changes
+
         if (!context.Lessons.Any())
         {
             context.Lessons.Add(
-            new Lesson
-            {
-                LessonID = 1,
-                Title = "Introduction to Programming",
-                Lector = 1,
-                SubjectID = "Informatica",
-                PdfLink = "https://google.com"
-            }
-        );
+                new Lesson
+                {
+                    LessonID = 1,
+                    Title = "Introduction to Programming",
+                    Lector = 1,
+                    SubjectID = "Informatica",
+                    PdfLink = "https://google.com"
+                }
+            );
         }
-        
+
         context.SaveChanges();
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     }
     catch (Exception ex)
     {
         var logger = services.GetRequiredService<ILogger<Program>>();
         logger.LogError(ex, "An error occurred while seeding the DB.");
     }
-
-    
 }
 
 
