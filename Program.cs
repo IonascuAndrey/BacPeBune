c3559f1f61866f9b99d4213b7b2946acbb6b4e74
matei21 <43905663+matei21@users.noreply.github.com>
2025-06-07 21:00:50 +0300

    New Update to Program.cs

--- Program.cs
+++ Program.cs
@@ -75,9 +75,103 @@ using (var scope = app.Services.CreateScope())
                     Title = "Introduction to Programming",
                     Lector = 1,
                     SubjectID = "Informatica",
-                    PdfLink = "https://google.com"
+                    PdfLink = "https://x.com"
                 }
             );
+        }
+
+        if (!context.Quizzes.Any()) {
+
+            context.Quizzes.Add(
+                new Quiz
+                {
+                    QuizID = 1,
+                    Name = "Quiz 1 for Lesson 1",
+                    Reward = 10,
+                    LessonID = 1,
+                    lesson = context.Lessons.FirstOrDefault(l => l.LessonID == 1),
+                }
+            );
+        }
+
+        if (!context.Questions.Any()) {
+            context.Questions.Add(
+                new Question
+                {
+                    QuestionID = 1,
+                    Text = "What is the main purpose of programming?",
+                    QuizID = 1,
+                    Quiz = context.Quizzes.FirstOrDefault(q => q.QuizID == 1)
+                }
+            );
+
+            context.Questions.Add(
+                new Question
+                {
+                    QuestionID = 2,
+                    Text = "What is the purpose of algorithms?",
+                    QuizID = 1,
+                    Quiz = context.Quizzes.FirstOrDefault(q => q.QuizID == 1)
+                }
+            );
+        }
+
+        if (!context.Answers.Any()) {
+            context.Answers.Add(
+                    new Answer
+                    {
+                        AnswerID = 1,
+                        Text = "To create software applications",
+                        QuestionID = 1,
+                        IsCorrect = true,
+                        question = context.Questions.FirstOrDefault(q => q.QuestionID == 1)
+                    }
+                );
+
+            context.Answers.Add(
+                new Answer
+                {
+                    AnswerID = 2,
+                    Text = "To write documentation",
+                    QuestionID = 1,
+                    IsCorrect = false,
+                    question = context.Questions.FirstOrDefault(q => q.QuestionID == 1)
+                }
+            );
+
+            context.Answers.Add(
+                new Answer
+                {
+                    AnswerID = 3,
+                    Text = "To create algorithms",
+                    QuestionID = 2,
+                    IsCorrect = true,
+                    question = context.Questions.FirstOrDefault(q => q.QuestionID == 2)
+                }
+            );
+
+            context.Answers.Add(
+                new Answer
+                {
+                    AnswerID = 4,
+                    Text = "To write code",
+                    QuestionID = 2,
+                    IsCorrect = false,
+                    question = context.Questions.FirstOrDefault(q => q.QuestionID == 2)
+                }
+            );
+
+            context.Answers.Add(
+                new Answer
+                {
+                    AnswerID = 5,
+                    Text = "To create user interfaces",
+                    QuestionID = 2,
+                    IsCorrect = false,
+                    question = context.Questions.FirstOrDefault(q => q.QuestionID == 2)
+                }
+            );
+
         }
 
         context.SaveChanges();
