8f830d385d20ff13969ce206bbb003d8e3647c27
Pacobaba <vrabiealexandru25@yahoo.ro>
2025-06-13 03:41:12 +0300

    just an insert

--- Program.cs
+++ Program.cs
@@ -57,7 +57,6 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
